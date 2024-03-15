0d27895a3731ee45b3cc2b2323349ba524135f00
Justin F. Hallett <thesin@southofheaven.org>
2024-03-15 09:46:05 -0600

    Add uninstall option, add link refresh using uninstall, add printer.cfg check

--- install.sh
+++ install.sh
@@ -40,19 +40,44 @@ function check_download {
     fi
 }
 
+function remove_links {
+    for path in "${KLIPPER_PATH}"/klippy/extras/ "${CONFIG_PATH}"/config/; do
+        echo "[UNINSTALL] Old links in ${path}";
+        for file in $(find "${KLIPPER_PATH}"/klippy/extras -type l -exec readlink -f {} \; | grep -F "${HOME}"/klipper-toolchanger); do
+            rm ${file};
+        done
+    done
+}
+
 function link_extension {
     echo "[INSTALL] Linking extension to Klipper..."
-    for file in "${INSTALL_PATH}"/klipper/extras/*.py; do ln -sfn "${file}" "${KLIPPER_PATH}/klippy/extras/"; done
+    for file in "${INSTALL_PATH}"/klipper/extras/*.py; do
+        ln -sfn ${file} "${KLIPPER_PATH}"/klippy/extras/;
+    done
 }
 
 function link_macros {
     echo "[INSTALL] Linking macros to Klipper..."
-    for file in "${INSTALL_PATH}"/macros/*.cfg; do ln -sfn "${file}" "${CONFIG_PATH}/config/"; done
+    for file in "${INSTALL_PATH}"/macros/*.cfg; do
+        ln -sfn ${file} "${CONFIG_PATH}"/config/;
+    done
 }
 
 function copy_examples {
     echo "[INSTALL] Copying in examples to Klipper..."
-    for file in "${INSTALL_PATH}"/examples/*.cfg; do cp -n "${file}" "${CONFIG_PATH}/config/"; done
+    for file in "${INSTALL_PATH}"/examples/*.cfg; do
+        cp -n ${file} "${CONFIG_PATH}"/config/;
+    done
+}
+
+function check_includes {
+    echo "[CHECK-INSTALL] Checking for missing includes..."
+    for file in "${INSTALL_PATH}"/macros/*.cfg; do
+        filename=$(basename ${file});
+        if !grep -q "include ${filename}" "${CONFIG_PATH}"/config/printer.cfg; then
+            echo " - Missing [include ${filename}] in printer.cfg"
+        fi
+    done
 }
 
 function restart_klipper {
@@ -62,12 +87,28 @@ function restart_klipper {
 
 printf "\n======================================\n"
 echo "- Klipper toolchanger install script -"
+echo "-                                    -"
+echo "- If you are upgrading maybe sure to -"
+echo "- you check for changes in the user  -"
+echo "- config files                       -"
 printf "======================================\n\n"
 
+install=1;
+if [ $1 == 'uninstall' ]; then
+    install=0;
+fi
+
 # Run steps
-preflight_checks
-check_download
-link_extension
-link_macros
-copy_examples
-restart_klipper
+if [ $install -eq 1]; then
+    preflight_checks
+    check_download
+fi
+
+remove_links
+
+if [ $install -eq 1]; then
+    link_extension
+    link_macros
+    copy_examples
+    restart_klipper
+fi
