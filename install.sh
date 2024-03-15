10ee6b3c2f1bf981a88c29339068dc58eb315057
Justin F. Hallett <thesin@southofheaven.org>
2024-03-15 10:43:54 -0600

    Add repo origin check, cleanup output

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+REPO="https://github.com/StealthChanger/klipper-toolchanger.git"
 CONFIG_PATH="${HOME}/printer_data"
 KLIPPER_PATH="${HOME}/klipper"
 INSTALL_PATH="${HOME}/klipper-toolchanger"
@@ -14,11 +15,12 @@ function preflight_checks {
     fi
 
     if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F 'klipper.service')" ]; then
-        printf "[PRE-CHECK] Klipper service found! Continuing...\n\n"
+        echo "[PRE-CHECK] Klipper service found!"
     else
         echo "[ERROR] Klipper service not found, please install Klipper first!"
         exit -1
     fi
+    echo
 }
 
 function check_download {
@@ -26,70 +28,111 @@ function check_download {
     installdirname="$(dirname ${INSTALL_PATH})"
     installbasename="$(basename ${INSTALL_PATH})"
 
+    doclone=0
     if [ ! -d "${INSTALL_PATH}" ]; then
-        echo "[DOWNLOAD] Downloading repository..."
-        if git -C $installdirname clone https://github.com/StealthChanger/klipper-toolchanger.git $installbasename; then
+        doclone=1
+    else
+        if [ "$(cd "${INSTALL_PATH}" && git remote get-url origin)" != "${REPO}" ]; then
+            echo -n "[DOWNLOAD] Removing incorrect repository..."
+            rm -rf "${INSTALL_PATH}"
+            doclone=1
+            echo " complete!"
+        fi
+    fi
+
+    if [ $doclone -gt 0 ]; then
+        echo -n "[DOWNLOAD] Cloning repository..."
+        if git -C $installdirname clone ${REPO} $installbasename; then
             chmod +x ${INSTALL_PATH}/install.sh
-            printf "[DOWNLOAD] Download complete!\n\n"
+            echo " complete!"
         else
-            echo "[ERROR] Download of git repository failed!"
+            echo " failed!"
             exit -1
         fi
     else
-        printf "[DOWNLOAD] repository already found locally. Continuing...\n\n"
+        echo "[DOWNLOAD] repository already found locally."
     fi
+    echo
 }
 
 function remove_links {
-    echo "[UNINSTALL] Remove old links..."
+    echo -n "[UNINSTALL] Remove old links..."
     for path in "${KLIPPER_PATH}"/klippy/extras/ "${CONFIG_PATH}"/config/; do
         for file in $(find "${path}" -type l); do
             if readlink -f "${file}" | grep -q "${INSTALL_PATH}"; then
-              rm ${file};
+                if ! rm ${file}; then
+                    echo " failed!"
+                    exit -1
+                fi
             fi
         done
     done
+    echo " complete!"
     echo
 }
 
 function link_extension {
-    echo "[INSTALL] Linking extension to Klipper..."
+    echo -n "[INSTALL] Linking extension to Klipper..."
     for file in "${INSTALL_PATH}"/klipper/extras/*.py; do
-        ln -sfn ${file} "${KLIPPER_PATH}"/klippy/extras/;
+        if ! ln -sfn ${file} "${KLIPPER_PATH}"/klippy/extras/; then
+            echo " failed!"
+            exit -1
+        fi
     done
+    echo " complete!"
     echo
 }
 
 function link_macros {
-    echo "[INSTALL] Linking macros to Klipper..."
+    echo -n "[INSTALL] Linking macros to Klipper..."
     for file in "${INSTALL_PATH}"/macros/*.cfg; do
-        ln -sfn ${file} "${CONFIG_PATH}"/config/;
+        if ! ln -sfn ${file} "${CONFIG_PATH}"/config/; then
+            echo " failed!"
+            exit -1
+        fi
     done
+    echo " complete!"
     echo
 }
 
 function copy_examples {
-    echo "[INSTALL] Copying in examples to Klipper..."
+    echo -n "[INSTALL] Copying in examples to Klipper..."
     for file in "${INSTALL_PATH}"/examples/*.cfg; do
-        cp -n ${file} "${CONFIG_PATH}"/config/;
+        if ! cp -n ${file} "${CONFIG_PATH}"/config/; then
+            echo " failed!"
+            exit -1
+        fi
     done
+    echo " complete!"
     echo
 }
 
 function check_includes {
-    echo "[CHECK-INSTALL] Checking for missing includes..."
+    echo -n "[CHECK-INSTALL] Checking for missing includes..."
+    found=0
     for file in "${INSTALL_PATH}"/macros/*.cfg; do
         filename=$(basename ${file});
         if ! grep -q "include ${filename}" "${CONFIG_PATH}"/config/printer.cfg; then
+            if [ $found -lt 1 ]; then
+                echo " found!"
+                found=1
+            fi
             echo " - Missing [include ${filename}] in printer.cfg"
         fi
     done
+    if [ $found -lt 1 ]; then
+        echo " complete!"
+    fi
     echo
 }
 
 function restart_klipper {
-    echo "[POST-INSTALL] Restarting Klipper..."
-    sudo systemctl restart klipper
+    echo -n "[POST-INSTALL] Restarting Klipper..."
+    if ! sudo systemctl restart klipper; then
+        echo " failed!"
+        exit -1
+    fi
+    echo " complete!"
     echo
 }
 
