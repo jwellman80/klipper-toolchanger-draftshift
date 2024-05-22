c4d2a97b2cbd54f6d8885cc63de997561e67b7c0
Justin F. Hallett <thesin@southofheaven.org>
2024-05-22 09:52:43 -0600

    Cheery pick: Remember Z adjust between tool changes.

--- klipper/extras/toolchanger.py
+++ klipper/extras/toolchanger.py
@@ -255,7 +255,7 @@ class Toolchanger:
         if select_tool:
             self._configure_toolhead_for_tool(select_tool)
             self.run_gcode('after_change_gcode', self.after_change_gcode, {})
-            self._set_tool_gcode_offset(select_tool)
+            self._set_tool_gcode_offset(select_tool, 0.0)
 
         if should_run_initialize:
             if self.status == STATUS_INITIALIZING:
@@ -284,8 +284,9 @@ class Toolchanger:
                 return
 
             self.status = STATUS_CHANGING
-
+        toolhead_position = self.gcode_move.get_status()['position']
         gcode_position = self.gcode_move.get_status()['gcode_position']
+        extra_z_offset = toolhead_position[2] - gcode_position[2] - self.active_tool.gcode_z_offset if self.active_tool else 0.0
 
         extra_context = {
             'dropoff_tool': self.active_tool.name if self.active_tool else None,
@@ -321,7 +322,7 @@ class Toolchanger:
             "RESTORE_GCODE_STATE NAME=_toolchange_state MOVE=0")
         # Restore state sets old gcode offsets, fix that.
         if tool is not None:
-            self._set_tool_gcode_offset(tool)
+            self._set_tool_gcode_offset(tool, extra_z_offset)
 
         if not force_pickup:
             self.status = STATUS_READY
@@ -374,7 +375,7 @@ class Toolchanger:
         if self.active_tool:
             self.active_tool.activate()
 
-    def _set_tool_gcode_offset(self, tool):
+    def _set_tool_gcode_offset(self, tool, extra_z_offset):
         if tool is None:
             return
         if tool.gcode_x_offset is None and tool.gcode_y_offset is None and tool.gcode_z_offset is None:
@@ -385,7 +386,7 @@ class Toolchanger:
         if tool.gcode_y_offset is not None:
             cmd += ' Y=%f' % (tool.gcode_y_offset,)
         if tool.gcode_z_offset is not None:
-            cmd += ' Z=%f' % (tool.gcode_z_offset,)
+            cmd += ' Z=%f' % (tool.gcode_z_offset + extra_z_offset,)
         self.gcode.run_script_from_command(cmd)
         mesh = self.printer.lookup_object('bed_mesh')
         if mesh and mesh.get_mesh():
